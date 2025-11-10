d7f0b7bb70a66a00fb8643ad8703dc6efae73574
navan260 <navaneeth.v.26@gmail.com>
2025-11-10 09:43:34 +0530

    Upload route

--- backend/main.py
+++ backend/main.py
@@ -2,13 +2,20 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import datetime
-
-from db import crud_operations
+from db.supabase import patient_operations
+from fastapi.staticfiles import StaticFiles  # <-- 1. IMPORT THIS
+from pathlib import Path
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="backend/templates")
+BASE_DIR = Path(__file__).resolve().parent
 
+app.mount(
+    "/static",
+    StaticFiles(directory=str(Path(BASE_DIR, "static"))),
+    name="static"
+)
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
@@ -25,18 +32,36 @@ async def read_patient_form(
     patientName: str = Form(...),
     patientPhone : str = Form(...),
     patientEmail: str = Form(...),
-    patientAddress: str = Form(...)
+    patientAddress: str = Form(...),
+    diseaseName: str = Form(...)
 ):
     print(patientName)
     context = {
         "request": request,
-        "success": patientName
+        "success": patientName,
+        "name": patientName,
     }
-    crud_operations.insert_patient(
+    patient_operations.insert_patient(
         name=patientName,
         phone=patientPhone,
         email=patientEmail,
         address=patientAddress,
-        date_added=datetime.datetime.now()
+        disease_name=diseaseName,
+        disease_id="3293279327"
     )
+    return templates.TemplateResponse(name="success.html", context=context)
+
+@app.get("/upload",response_class=HTMLResponse)
+async def file_upload(request: Request):
+    context = {
+        "request": request
+    }
+    return templates.TemplateResponse(name="upload.html", context=context)
+
+@app.post("/patient/upload", response_class=HTMLResponse)
+async def upload_file_info(request: Request):
+    context = {
+        "request": request
+    }
+    
     return templates.TemplateResponse(name="success.html", context=context)
